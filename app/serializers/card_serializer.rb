256b929dab0105e20d605541f9b0e71b7223bc7f
Charlie Cook <turingbombe@gmail.com>
2016-10-10 19:20:01 -0400

    added cards to collection information. Changed card serializer

--- app/serializers/card_serializer.rb
+++ app/serializers/card_serializer.rb
@@ -1,4 +1,4 @@
 class CardSerializer < ActiveModel::Serializer
   # cache key: 'card'
-  attributes :id, :multiverse_id, :artist,:cmc,:colors,:flavor,:image_url,:mana_cost, :name, :number, :original_text, :original_type, :power, :printings,:rarity, :set_name, :subtypes, :card_text, :toughness, :card_type, :types,:card_set_id
+  attributes :id,:cmc,:colors,:flavor,:image_url,:mana_cost, :name,:original_text, :original_type, :power,:rarity, :set_name, :card_text, :toughness, :card_type, :card_set_id
 end
--- app/serializers/collection_serializer.rb
+++ app/serializers/collection_serializer.rb
@@ -1,4 +1,5 @@
 class CollectionSerializer < ActiveModel::Serializer
   attributes :id, :name, :card_ids, :user_id
   has_many :decks
+  has_many :cards
 end
