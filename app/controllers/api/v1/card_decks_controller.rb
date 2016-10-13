7a3f38270ae5e26730cd38b8e700f9d595669564
Charlie Cook <turingbombe@gmail.com>
2016-10-13 09:18:52 -0400

    added card-decks controller

--- app/controllers/api/v1/card_decks_controller.rb
+++ app/controllers/api/v1/card_decks_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class CardDecksController < ApplicationController
     	def update
-    		deck = @current_user.decks.find(params["deck_id"])
+    		deck = Deck.find(params["deck_id"])
     		deck.cards << Card.find(params["card_id"])
     		deck.save
     		render json: deck
